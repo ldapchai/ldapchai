904f63718ac2ce41414007dc9c986d5263860bad
James Albright <jalbr74@gmail.com>
2015-10-12 11:00:26 -0600

    Removed the maven dependency on the NMAS Toolkit.  So now when the setting: EDIRECTORY_ENABLE_NMAS is set to true, the validator will check to make sure the NMAS Toolkit library is available on the classpath.

---
+++ src/external/java/com/novell/security/nmas/jndi/ldap/ext/GetPwdPolicyInfoRequest.java
@@ -0,0 +1,53 @@
+package com.novell.security.nmas.jndi.ldap.ext;
+
+/**
+ * This class has been stubbed out so LDAP Chai has something to compile against.
+ * NMASToolkit.jar must exist on the target system in order to use the NMAS features.
+ */
+public final class GetPwdPolicyInfoRequest extends java.lang.Object implements javax.naming.ldap.ExtendedRequest {
+	private static final long serialVersionUID = 1L;
+
+	/** Empty implementation */
+	public GetPwdPolicyInfoRequest() {
+		super();
+	}
+
+	/** Empty implementation */
+	public GetPwdPolicyInfoRequest(java.lang.String p0, java.lang.String p1) {
+		super();
+	}
+
+	/** Empty implementation */
+	public java.lang.String getID() {
+		return null;
+	}
+
+	/** Empty implementation */
+	public javax.naming.ldap.ExtendedResponse createExtendedResponse(java.lang.String p0, byte[] p1, int p2, int p3) throws javax.naming.NamingException {
+		return null;
+	}
+
+	/** Empty implementation */
+	public byte[] getEncodedValue() {
+		return null;
+	}
+
+	/** Empty implementation */
+	public void setTreeName(java.lang.String p0) {
+	}
+
+	/** Empty implementation */
+	public void setObjectDN(java.lang.String p0) {
+	}
+
+	/** Empty implementation */
+	public java.lang.String getTreeName() {
+		return null;
+	}
+
+	/** Empty implementation */
+	public java.lang.String getObjectDN() {
+		return null;
+	}
+
+}
---
+++ src/external/java/com/novell/security/nmas/mgmt/NMASChallengeResponse.java
@@ -0,0 +1,18 @@
+package com.novell.security.nmas.mgmt;
+
+/**
+ * This class has been stubbed out so LDAP Chai has something to compile against.
+ * NMASToolkit.jar must exist on the target system in order to use the NMAS features.
+ */
+public final class NMASChallengeResponse extends java.lang.Object {
+
+	public static final int[] METHOD_ID = null;
+	public static final java.lang.String CHALLENGE_RESPONSE_QUESTIONS_TAG = null;
+	public static final java.lang.String CHALLENGE_RESPONSE_DATA_TAG = null;
+
+	/** Empty implementation */
+	public NMASChallengeResponse() {
+		super();
+	}
+
+}
